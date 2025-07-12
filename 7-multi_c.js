147f7cdff9c543bba1b88acf2522daca5bce97fc
noel knowles <coalzcoley@gmail.com>
2025-07-12 20:02:06 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,11 @@
+const x = Number(process.argv[2]);
+
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
